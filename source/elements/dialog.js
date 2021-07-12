e26196d692dea34d2f5e817a7354261e16e23d50
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-07-12 15:40:32 -0400

    update build

--- source/elements/dialog.js
+++ source/elements/dialog.js
@@ -1,5 +1,9 @@
+import dialog from '../templates/dialog.js';
+
 export class Dialog extends Quantum {
     constructor() {
         super();
     }
 }
+
+Dialog.define('quantum-dialog', dialog);
--- source/main.js
+++
@@ -1,4 +0,0 @@
-import { Dialog } from './elements/dialog.js';
-import dialog from './templates/dialog.js';
-
-Dialog.define('quantum-dialog', dialog);
